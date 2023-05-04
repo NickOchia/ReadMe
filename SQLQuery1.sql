c3fa674a399b8a41212a8efa8e4ea9614f42bae0
NickOchia <132538962+NickOchia@users.noreply.github.com>
2023-05-03 21:52:17 -0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,22 @@
+--INSERT INTO EmployeeDemographics VALUES
+--(1002, 'Shawn', 'Ellaso', 32, 'Male'),
+--(1003, 'Chuck', 'Trillanes', 40, 'Male'),
+--(1004, 'Charles', 'Trillanes', 34, 'Male'),
+--(1005, 'Gael', 'Vallery', 33, 'Female'),
+--(1006, 'Tracy', 'Ponaman', 28, 'Female'),
+--(1007, 'Greg', 'Ponaman', 36, 'Male'),
+--(1008, 'Ulaine', 'Castillio', 24, 'Female'),
+--(1009, 'Mikha', 'Magbanua', 25, 'Female'),
+--(1010, 'Nick', 'Ochia', 25, 'Male')
+
+INSERT INTO EmployeeSalary VALUES
+(1001, 'Medical Records', 3200),
+(1002, 'Caregiver', 4000),
+(1003, 'Medical Director', 4500),
+(1004, 'Assistant', 4300),
+(1005, 'CNA', 3500),
+(1006, 'Medical Billing', 5000),
+(1007, 'Electricity', 6100),
+(1008, 'Medical Assistant', 3000),
+(1009, 'Caregiver', 4200),
+(1010, 'Model', 4200)
---
+++ SQLQuery4.sql
@@ -0,0 +1,7 @@
+/*
+Select Statement
+*, Top, Distinct, Count, As, Max, Min, Avg
+*/
+
+SELECT *
+FROM SQLTutorial.dbo.EmployeeSalary
